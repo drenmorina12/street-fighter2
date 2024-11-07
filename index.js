fcc2607789744c6540e6dc7abe00b0d996511e22
Dren Morina <drenmorina321@gmail.com>
2024-11-07 14:01:41 +0100

    Set canvas sizing and add some styling

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const GameViewport = {
+  WIDTH: 384,
+  HEIGHT: 224,
+  SCALE: 3,
+};
+
 window.onload = function () {
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = GameViewport.WIDTH;
+  canvas.height = GameViewport.HEIGHT;
+
+  ctx.strokeStyle = "yellow";
+  ctx.moveTo(0, 0);
+  ctx.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+  ctx.moveTo(GameViewport.WIDTH, 0);
+  ctx.lineTo(0, GameViewport.HEIGHT);
+  ctx.stroke();
+
   console.log("Document is ready!");
 };
