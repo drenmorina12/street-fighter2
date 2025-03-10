1229c5ae342517e5a42502d7ca67ad764087021b
Dren Morina <drenmorina321@gmail.com>
2025-03-10 15:36:31 +0100

    Add fighter class and module

---
+++ src/fighter.js
@@ -0,0 +1,23 @@
+export class Fighter {
+  constructor(name, position, velocity) {
+    this.name = name;
+    this.image = new Image();
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  update(ctx) {
+    this.position.x += this.velocity.x;
+
+    if (
+      this.position.x > ctx.canvas.width - this.image.width ||
+      this.position.x < 0
+    ) {
+      this.velocity.x = -this.velocity.x;
+    }
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
