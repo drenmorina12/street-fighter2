6686fb9f2f94b21971c784379b1ba96a468a6ba7
Dren Morina <drenmorina321@gmail.com>
2025-03-11 01:34:51 +0100

    Add frames and animationFrame properties and update sprites based on current frame

--- src/entities/fighters/Fighter.js
+++ src/entities/fighters/Fighter.js
@@ -2,22 +2,42 @@ export class Fighter {
   constructor({ name, position, velocity }) {
     this.name = name;
     this.image = new Image();
+    this.frames = new Map();
     this.position = position;
     this.velocity = velocity;
+    this.animationFrame = 1;
   }
 
-  update(secondsPassed, ctx) {
-    this.position.x += this.velocity.x * secondsPassed;
+  update(time, ctx) {
+    const [, , width] = this.frames.get(`forwards-${this.animationFrame}`);
 
-    if (
-      this.position.x > ctx.canvas.width - this.image.width ||
-      this.position.x < 0
-    ) {
+    this.animationFrame++;
+    if (this.animationFrame > 6) {
+      this.animationFrame = 1;
+    }
+
+    this.position.x += this.velocity.x * time.secondsPassed;
+
+    if (this.position.x > ctx.canvas.width - width || this.position.x < 0) {
       this.velocity.x = -this.velocity.x;
     }
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, this.position.x, this.position.y);
+    const [x, y, width, height] = this.frames.get(
+      `forwards-${this.animationFrame}`
+    );
+
+    ctx.drawImage(
+      this.image,
+      x,
+      y,
+      width,
+      height,
+      this.position.x,
+      this.position.y,
+      width,
+      height
+    );
   }
 }
