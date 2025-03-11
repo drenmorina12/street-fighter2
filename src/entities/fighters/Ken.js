7be2d294e67574c6ae4838949aa451d17c0fd082
Dren Morina <drenmorina321@gmail.com>
2025-03-11 01:33:48 +0100

    Add forward frame coordinates for Ken and Ryu

--- src/entities/fighters/Ken.js
+++ src/entities/fighters/Ken.js
@@ -5,5 +5,14 @@ export class Ken extends Fighter {
     super({ name: "Ken", position, velocity });
 
     this.image = document.querySelector('img[alt="ken"]');
+
+    this.frames = new Map([
+      ["forwards-1", [8, 872, 53, 83]],
+      ["forwards-2", [70, 867, 60, 88]],
+      ["forwards-3", [140, 866, 64, 90]],
+      ["forwards-4", [215, 865, 63, 89]],
+      ["forwards-5", [288, 866, 54, 89]],
+      ["forwards-6", [357, 867, 50, 89]],
+    ]);
   }
 }
