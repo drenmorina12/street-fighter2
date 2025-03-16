e853022913873d2ee68cc9b7f8de5866deb7cd4d
Dren Morina <drenmorina321@gmail.com>
2025-03-16 14:23:12 +0100

    Add inputHandler to register and handle key events

---
+++ src/InputHandler.js
@@ -0,0 +1,21 @@
+const heldKeys = new Set();
+
+function handleKeyDown(event) {
+  event.preventDefault();
+
+  heldKeys.add(event.code);
+}
+
+function handleKeyUp(event) {
+  event.preventDefault();
+
+  heldKeys.delete(event.code);
+}
+
+export function registerKeyboardEvents() {
+  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener("keyup", handleKeyUp);
+}
+
+export const isKeyDown = (code) => heldKeys.has(code);
+export const isKeyUp = (code) => !heldKeys.has(code);
