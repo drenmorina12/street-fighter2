d1af191648a7af03bbcd6b00cd8ccbeff11dec27
Dren Morina <drenmorina321@gmail.com>
2025-03-18 17:09:16 +0100

    Add camera class, change stage position based on camera position, move camera

---
+++ src/Camera.js
@@ -0,0 +1,16 @@
+export class Camera {
+  constructor(x, y, fighters) {
+    this.position = { x, y };
+    this.fighters = fighters;
+
+    this.speed = 60;
+  }
+
+  update(time, ctx) {
+    this.position.x += this.speed * time.secondsPassed;
+
+    if (this.position.x > 640 || this.position.x < 256) {
+      this.speed = -this.speed;
+    }
+  }
+}
