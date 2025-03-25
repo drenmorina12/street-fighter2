9ed62237062228e44010c6e3404d2ab9ba067367
Dren Morina <drenmorina321@gmail.com>
2025-03-25 16:08:09 +0100

    Create BattleScene class
    remove the scene logic from the StreetFighterGame
    logic

--- src/StreetFighterGame.js
+++ src/StreetFighterGame.js
@@ -1,78 +1,21 @@
-import { Ken } from "./entities/fighters/Ken.js";
-import { KenStage } from "./entities/stage/KenStage.js";
-import { Ryu } from "./entities/fighters/Ryu.js";
-import { FpsCounter } from "./entities/overlays/FpsCounter.js";
-import { STAGE_MID_POINT, STAGE_PADDING } from "./constants/stage.js";
 import {
   pollGamepads,
   registerGamepadEvents,
   registerKeyboardEvents,
 } from "./engine/InputHandler.js";
-import { Shadow } from "./entities/fighters/Shadow.js";
-import { StatusBar } from "./entities/overlays/StatusBar.js";
-import { Camera } from "./engine/Camera.js";
 import { getContext } from "./utils/context.js";
-// HEREEEEEEE
+import { BattleScene } from "./scenes/BattleScene.js";
+
 export class StreetFighterGame {
   ctx = getContext();
-  fighters = [
-    new Ken({
-      playerId: 1,
-    }),
 
-    new Ryu({
-      playerId: 0,
-    }),
-  ];
-  camera = new Camera(
-    STAGE_MID_POINT + STAGE_PADDING - this.ctx.canvas.width / 2,
-    16,
-    this.fighters
-  );
   frameTime = {
     previous: 0,
     secondsPassed: 0,
   };
 
   constructor() {
-    this.stage = new KenStage();
-
-    this.fighters[0].opponent = this.fighters[1];
-    this.fighters[1].opponent = this.fighters[0];
-
-    this.entities = [
-      ...this.fighters.map((fighter) => new Shadow(fighter)),
-      ...this.fighters,
-    ];
-
-    this.overlays = [new StatusBar(this.fighters), new FpsCounter()];
-  }
-
-  update() {
-    this.camera.update(this.frameTime, this.ctx);
-    this.stage.update(this.frameTime, this.ctx);
-
-    for (const entity of this.entities) {
-      entity.update(this.frameTime, this.ctx, this.camera);
-    }
-
-    for (const overlay of this.overlays) {
-      overlay.update(this.frameTime, this.ctx, this.camera);
-    }
-  }
-
-  draw() {
-    this.stage.drawBackground(this.ctx, this.camera);
-
-    for (const entity of this.entities) {
-      entity.draw(this.ctx, this.camera);
-    }
-
-    this.stage.drawForeground(this.ctx, this.camera);
-
-    for (const overlay of this.overlays) {
-      overlay.draw(this.ctx, this.camera);
-    }
+    this.scene = new BattleScene();
   }
 
   frame(time) {
@@ -84,8 +27,8 @@ export class StreetFighterGame {
     };
 
     pollGamepads();
-    this.update();
-    this.draw();
+    this.scene.update(this.frameTime, this.ctx);
+    this.scene.draw(this.ctx);
   }
 
   start() {
