db52fcd103b5fc6f02df747bfe62777b902149b8
Dren Morina <drenmorina321@gmail.com>
2025-03-25 13:30:01 +0100

    Allow fighters to attack shile walking

--- src/entities/fighters/Fighter.js
+++ src/entities/fighters/Fighter.js
@@ -370,6 +370,20 @@ export class Fighter {
       this.changeState(FighterState.CROUCH_DOWN);
     }
 
+    if (control.isLightPunch(this.playerId)) {
+      this.changeState(FighterState.LIGHT_PUNCH);
+    } else if (control.isMediumPunch(this.playerId)) {
+      this.changeState(FighterState.MEDIUM_PUNCH);
+    } else if (control.isHeavyPunch(this.playerId)) {
+      this.changeState(FighterState.HEAVY_PUNCH);
+    } else if (control.isLightKick(this.playerId)) {
+      this.changeState(FighterState.LIGHT_KICK);
+    } else if (control.isMediumKick(this.playerId)) {
+      this.changeState(FighterState.MEDIUM_KICK);
+    } else if (control.isHeavyKick(this.playerId)) {
+      this.changeState(FighterState.HEAVY_KICK);
+    }
+
     this.direction = this.getDirection();
   }
 
@@ -381,6 +395,21 @@ export class Fighter {
     } else if (control.isDown(this.playerId)) {
       this.changeState(FighterState.CROUCH_DOWN);
     }
+
+    if (control.isLightPunch(this.playerId)) {
+      this.changeState(FighterState.LIGHT_PUNCH);
+    } else if (control.isMediumPunch(this.playerId)) {
+      this.changeState(FighterState.MEDIUM_PUNCH);
+    } else if (control.isHeavyPunch(this.playerId)) {
+      this.changeState(FighterState.HEAVY_PUNCH);
+    } else if (control.isLightKick(this.playerId)) {
+      this.changeState(FighterState.LIGHT_KICK);
+    } else if (control.isMediumKick(this.playerId)) {
+      this.changeState(FighterState.MEDIUM_KICK);
+    } else if (control.isHeavyKick(this.playerId)) {
+      this.changeState(FighterState.HEAVY_KICK);
+    }
+
     this.direction = this.getDirection();
   }
 
