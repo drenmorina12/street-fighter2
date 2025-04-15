9557e05bb8a0cebf6a66ab650d873b4c32eee414
Dren Morina <drenmorina321@gmail.com>
2025-04-15 23:54:46 +0200

    Add entity functions to new EntityList class

---
+++ src/engine/EntityList.js
@@ -0,0 +1,30 @@
+export class EntityList {
+  entities = [];
+
+  add(EntityClass, ...args) {
+    this.entities.push(new EntityClass(...args, this.removeEntity.bind(this)));
+  }
+
+  remove(entity) {
+    const index = this.entities.indexOf(entity);
+
+    if (index < 0) {
+      return;
+    }
+
+    this.entities.splice(index, 1);
+    // this.entities = this.entities.filter((thisEntity) => thisEntity !== entity);
+  }
+
+  update(time, ctx, camera) {
+    for (const entity of this.entities) {
+      entity.update(time, ctx, camera);
+    }
+  }
+
+  draw(ctx, camera) {
+    for (const entity of this.entities) {
+      entity.draw(ctx, camera);
+    }
+  }
+}
